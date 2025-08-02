44d45a5c7e382ec1c7b980d2136f3effb63f7d31
ZaidIslam1 <121275717+ZaidIslam1@users.noreply.github.com>
2025-08-01 22:55:57 -0600

    Signup functionality added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,14 @@
-export const signup = async (req, res) => {
+import User from "../models/user.model.js";
+
+export const signup = async (req, res, next) => {
     try {
-        res.send("signup controller");
+        const { name, email, password } = req.body;
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ success: false, message: "User already exists" });
+        }
+        const user = await User.create({ name, email, password });
+        res.status(201).json({ success: true, user, message: "User created successfully" });
     } catch (error) {
         next(error);
     }
