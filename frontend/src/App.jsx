db20495bb4173b5b2c499a756a3b297ea95e5d10
ZaidIslam1 <121275717+ZaidIslam1@users.noreply.github.com>
2025-08-15 00:14:29 -0600

    Frontend Signup UI completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/home/Homepage";
+import LoginPage from "./pages/auth/LoginPage";
+import SignupPage from "./pages/auth/SignupPage";
+import Navbar from "./components/Navbar";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import CartPage from "./pages/CartPage";
+
+function App() {
+    return (
+        <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+            {/* Background gradient */}
+            <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]" />
+
+            {/* Content */}
+            <div className="relative z-10 pt-20">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignupPage />} />
+                    <Route path="/admin-dashboard" element={<AdminDashboard />} />
+                    <Route path="/cart" element={<CartPage />} />
+                </Routes>
+            </div>
+        </div>
+    );
+}
+
+export default App;
