60f5d59878e6610fd5fd05731756af969c9fd4f7
ZaidIslam1 <121275717+ZaidIslam1@users.noreply.github.com>
2025-08-19 22:20:34 -0600

    Server.js static assets changed

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use((err, req, res, next) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend", "dist")));
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
     });
